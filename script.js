0897b48e97fc814de3ede91bdbe88726d60d1b4b
ayush-geeks <ayushkhandelwal0608@gmail.com>
2022-05-09 21:03:31 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const container = document.querySelector(".container");
+
+const input = document.querySelector(".form input");
+const generateBtn = document.querySelector(".form button");
+
+const qrImg = document.querySelector(".qr-code img");
+
+generateBtn.addEventListener("click", function () {
+    const inputVal = input.value;
+    console.log(inputVal);
+    if (!inputVal) {
+        container.classList.remove("active");
+        return;
+    }
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${inputVal}`;
+    container.classList.add("active");
+});
